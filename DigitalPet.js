e173c86fb8dac572f37e4787294d0b0a179e33c7
Shane <shanegates@hotmail.com>
2019-05-15 18:15:19 -0700

    basic digital pet

---
+++ DigitalPet.js
@@ -0,0 +1,48 @@
+const DigitalPet = function () {
+  this.hungry = false;
+  this.sleepy = false;
+  this.bored = true;
+  this.age = 0;
+};
+
+DigitalPet.prototype.stats = function () {
+  console.log(`Hungry: ${this.hungry}`);
+  console.log(`Sleepy: ${this.sleepy}`);
+  console.log(`Bored: ${this.bored}`);
+  console.log(`Age: ${this.age}`);
+};
+
+DigitalPet.prototype.feed = function () {
+  if (this.hungry === true) {
+    this.hungry = false;
+    this.sleepy = true;
+    console.log("That was yummy!");
+  }
+  else {
+    console.log("No thanks, I'm full.");
+  }
+};
+
+DigitalPet.prototype.sleep = function () {
+  if (this.sleepy === true) {
+    this.sleepy = false;
+    this.bored = true;
+    console.log("ZZzzZZZzzZZz~~");
+  }
+  else {
+    console.log("No way! I'm not tired!");
+  }
+};
+
+DigitalPet.prototype.play = function () {
+  if (this.bored === true) {
+    this.bored = false;
+    this.hungry = true;
+    console.log("Yay! Let's play!");
+  }
+  else {
+    console.log("Not right now. Maybe later?");
+  }
+};
+
+module.exports = DigitalPet;
---
+++ test.js
@@ -0,0 +1,10 @@
+const DigitalPet = require("./DigitalPet");
+
+const cat = new DigitalPet("Mochi");
+
+cat.stats();
+
+cat.play();
+cat.play();
+cat.feed();
+cat.sleep();
