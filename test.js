6a07063717d807b3307c2fc0fd5e3b51fd204ab9
Shane <shanegates@hotmail.com>
2019-05-15 21:15:54 -0700

    formatting. renamed to playerOne & playerTwo

--- test.js
+++ test.js
@@ -1,32 +1,33 @@
 const DigitalPet = require("./DigitalPet");
 
-const cat = new DigitalPet("Mochi", 12, 8, 100, "MEOW");
-const dog = new DigitalPet("Cho", 8, 10, 100, "WOOF");
+const playerOne = new DigitalPet("Mochi", 12, 8, 100, "MEOW");
+const playerTwo = new DigitalPet("Cho", 8, 10, 100, "WOOF");
 
-console.log();
+console.log("\nINTRO:\n");
 
-cat.stats();
-dog.stats();
+playerOne.stats();
+playerTwo.stats();
 
 console.log("----------\n");
 
-cat.play();
-cat.play();
-cat.feed();
-cat.sleep();
+playerOne.play();
+playerOne.play();
+playerOne.feed();
+playerOne.sleep();
+
+console.log();
 
 let turn = 0;
-while (cat.isAlive() && dog.isAlive()) {
+while (playerOne.isAlive() && playerTwo.isAlive()) {
     turn += 1;
     console.log("-------------\n");
     console.log(`TURN: ${turn}\n`);
 
-    cat.attack(dog);
-    dog.attack(cat);
+    playerOne.attack(playerTwo);
+    playerTwo.attack(playerOne);
 
-    // console.log("\n-------------\n");
     console.log();
 
-    cat.stats();
-    dog.stats();
+    playerOne.stats();
+    playerTwo.stats();
 }
